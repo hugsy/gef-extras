34eca6421fcaa341340dcadd7358d10d61858102
hugsy <hugsy@blah.cat>
2017-05-12 10:19:29 -0700

    structured display of arguments

--- ftrace.py
+++ ftrace.py
@@ -6,6 +6,7 @@
 # Load with:
 # gef> source /path/to/this/script.py
 #
+import collections
 
 class FtraceEnterBreakpoint(gdb.Breakpoint):
     def __init__(self, location, nb_args, *args, **kwargs):
@@ -15,7 +16,9 @@ class FtraceEnterBreakpoint(gdb.Breakpoint):
         return
 
     def stop(self):
-        regs = [ "{:#x}".format(get_register(r)) for r in current_arch.function_parameters[:self.nb_args] ]
+        regs = collections.OrderedDict()
+        for r in current_arch.function_parameters[:self.nb_args]:
+            regs[r] = get_register(r)
         self.retbp = FtraceExitBreakpoint(location=self.location, regs=regs)
         return False
 
@@ -27,24 +30,53 @@ class FtraceExitBreakpoint(gdb.FinishBreakpoint):
         return
 
     def stop(self):
-        retval = "(void)" if self.return_value is None else "{:#x}".format(long(self.return_value))
-        output = get_gef_setting("ftrace.output") or "/dev/stderr"
-        with open(output, "a") as f:
-            f.write("{}({}) = {}\n".format(self.args["location"], ','.join(self.args["regs"]), retval))
+        if self.return_value:
+            retval = "{:#x}".format(long(self.return_value))
+        else:
+            retval = get_register(current_arch.return_register)
+
+        output = get_gef_setting("ftrace.output")
+        mode = "a"
+        if output is None:
+            output = "/dev/stderr"
+            mode = "w"
+
+        with open(output, "w") as fd:
+            fd.write("{}() = {} {{\n".format(self.args["location"], retval))
+            for reg in self.args["regs"].keys():
+                regval = self.args["regs"][reg]
+                fd.write("\t{} {} {}\n".format(reg, right_arrow, right_arrow.join(DereferenceCommand.dereference_from(regval))))
+            fd.write("}\n")
+            fd.flush()
         return False
 
 class FtraceCommand(GenericCommand):
     """Tracks a function given in parameter for arguments and return code."""
     _cmdline_ = "ftrace"
-    _syntax_  = "{:s} <function_name>".format(_cmdline_)
+    _syntax_  = "{:s} <function_name1>,<nb_args1> [<function_name2>,<nb_args2> ...]".format(_cmdline_)
 
     def do_invoke(self, args):
-        if len(args) < 1: return
+        if len(args) < 1:
+            self.usage()
+            return
+
+        self.bkps = []
+
         for item in args:
             funcname, nb_args = item.split(",")
-            FtraceEnterBreakpoint(funcname, int(nb_args))
-            ok("added '{}' to ltrace list".format(funcname))
+            self.bkps.append(FtraceEnterBreakpoint(funcname, int(nb_args)))
+            ok("added '{}()' (with {} args) to tracking list".format(funcname, nb_args))
+
+        gdb.events.exited.connect(self.cleanup)
         return
+
+    def cleanup(self, events):
+        for bp in self.bkps:
+            bp.retbp.delete()
+            bp.delete()
+        gdb.events.exited.disconnect(self.cleanup)
+        return
+
 
 if __name__ == "__main__":
     register_external_command( FtraceCommand() )
