06a3939e52b7512737273197d26e4a82601439a2
hugsy <hugsy@blah.cat>
2017-10-15 20:06:29 -0700

    rewrote peekpointers.py to use gef already existing functions

--- peekpointers.py
+++ peekpointers.py
@@ -1,6 +1,4 @@
-
 class PeekPointers(GenericCommand):
-
     """
     Command to help find pointers belonging to other memory regions
     helpful in case of OOB Read when looking for specific pointers
@@ -24,105 +22,56 @@ class PeekPointers(GenericCommand):
     _cmdline_ = "peek-pointers"
     _syntax_  = "{:s} starting_address <object_name> <all>".format(_cmdline_)
 
-    
-    def _build_ranges(self, name=""):
-
-        maps = get_process_maps()
-        ranges = {}
-        for m in maps:
-
-            if not m.path:
-                continue
-            if name and name != m.path and not m.path.endswith(name):
-                continue
-            if m.path not in ranges:
-                ranges[m.path] = (1 << 128, 0)
-
-            start_m, end_m = ranges[m.path]
-            if m.page_start < start_m: 
-                start_m = m.page_start 
-            if m.page_end > end_m:
-                end_m = m.page_end 
-
-            ranges[m.path] = (start_m, end_m)
-                       
-        return ranges
-
-    def _get_name_maps(self):
-        names = []
-        for m in get_process_maps():
-            if m.path and m.path not in names:
-                names.append(m.path) 
-
-        print(names)
-        return names
-
-    def _resolve_symbol(self, addr):
-        try:
-            res = gdb.execute("x/gx 0x{:x}".format(addr), to_string=True)
-        except:
-            raise Exception()
-            
-        if "<" in res:
-            sym = res.split("<")[1].split(">")[0]
-            return sym
-        return ""
-
     @only_if_gdb_running
     def do_invoke(self, argv):
-        assert(len(argv) >= 1)
-         
-        addr = int(argv[0], 16)
-        incr = 0
+        argc = len(argv)
+        if argc not in (1, 2, 3):
+            self.usage()
+            return
 
-        unique = True
-        specific_name = ""
+        addr = lookup_address(int(argv[0], 16))
+        if (addr.value % DEFAULT_PAGE_SIZE):
+            err("<starting_address> must be aligned to a page")
+            return
 
-        if "all" in argv:
-            unique = False
+        unique = True if "all" not in argv else False
+        vmmap = get_process_maps()
 
-        if len(argv) >= 2 and argv[1] == "stack":
-            specific_name = "[stack]"
-        elif len(argv) >= 2 and argv[1] == "heap":
-            specific_name = "[heap]"
-        elif len(argv) >= 2 and argv[1] != "all": 
-            so_name = argv[1]
-            specific_name = argv[1]
-        ranges = self._build_ranges(specific_name)
-        read_fn = read_int_from_memory 
+        if argc >= 2 :
+            section_name = argv[1].lower()
+            if   section_name == "stack":
+                sections = [(s.path, s.page_start, s.page_end) for s in vmmap if s.path == "[stack]"]
+            elif section_name == "heap":
+                sections = [(s.path, s.page_start, s.page_end) for s in vmmap if s.path == "[heap]"]
+            elif section_name != "all":
+                sections = [ (s.path, s.page_start, s.page_end) for s in vmmap if section_name in s.path ]
+            else:
+                sections = [ (s.path, s.page_start, s.page_end) for s in vmmap ]
+        else:
+            sections = [ (s.path, s.page_start, s.page_end) for s in vmmap ]
 
-        incr = get_memory_alignment() # hack around GEF not checking for cases where the inferior does not have a name
+        while addr.valid:
+            addr_value = read_int_from_memory(addr.value)
 
-        assert(incr > 0)
-        
-        while ranges:
-            try:
-                v = read_fn(addr)
-            except:
-                print("Could not read from address 0x{:x}, stopping.".format(addr))
-                return
-            
-            for k in ranges:
-                if ranges[k][0] <= v < ranges[k][1]:
-                    sym = None
-                    # call resolve symbol in try catch block
-                    # to avoid cases where it is not a valid pointer
-                    try:
-                        sym = self._resolve_symbol(v)
-                    except:
+            if lookup_address(addr_value):
+                for i, section in enumerate(sections):
+                    name, start_addr, end_addr = section
+                    if start_addr <= addr_value < end_addr:
+                        sym = gdb_get_location_from_symbol(addr_value)
+                        sym = "<{:s}+{:04x}>".format(*sym) if sym else ''
+                        if name.startswith("/"):
+                            name = os.path.basename(name)
+                        elif len(name)==0:
+                            name = get_filename()
+
+                        ok(" Found pointer at 0x{:x} to 0x{:x} {:s} ('{:s}', perm: {:s})".format(addr.value, addr_value, sym, name, str(addr.section.permission), ))
+
+                        if unique:
+                            del sections[i]
                         break
-                    short_name = k
-                    if short_name[0] == "/":
-                        short_name = short_name.split("/")[-1]
-                    if sym:
-                        print("{:s} pointer at 0x{:x}, value 0x{:x} <{:s}>".format(short_name, addr, v, sym))
-                    else:
-                        print("{:s} pointer at 0x{:x}, value 0x{:x}".format(short_name, addr, v))
-                    if unique:
-                        del ranges[k]
-                    break
- 
-            addr += incr
+
+            addr = lookup_address(addr.value + current_arch.ptrsize)
+        return
 
 
 if __name__ == "__main__":
