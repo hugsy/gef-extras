75168696dda32c9fea5ac77e11c0d7cbceaa056d
hugsy <hugsy@blah.cat>
2018-01-21 18:58:24 -0800

    stack: if no older frame can be found, print an error with the reason why

--- stack.py
+++ stack.py
@@ -2,6 +2,8 @@ class CurrentFrameStack(GenericCommand):
     """Show the entire stack of the current frame."""
     _cmdline_ = "current-stack-frame"
     _syntax_  = "{:s}".format(_cmdline_)
+    _aliases_ = ["stack", "full-stack",]
+    _example_ = "{:s}".format(_cmdline_)
 
     @only_if_gdb_running
     def do_invoke(self, argv):
@@ -9,7 +11,9 @@ class CurrentFrameStack(GenericCommand):
         frame = gdb.selected_frame()
 
         if not frame.older():
-            warn("Cannot determine frame boundary")
+            reason = frame.unwind_stop_reason()
+            reason_str = gdb.frame_stop_reason_string( frame.unwind_stop_reason() )
+            warn("Cannot determine frame boundary, reason: {:s}".format(reason_str))
             return
 
         saved_ip = frame.older().pc()
@@ -21,10 +25,8 @@ class CurrentFrameStack(GenericCommand):
 
         for offset, address in enumerate(range(stack_lo, stack_hi, ptrsize)):
             pprint_str = DereferenceCommand.pprint_dereferenced(stack_lo, offset)
-
             if dereference(address) == saved_ip:
                 pprint_str += " " + Color.colorify("($savedip)", attrs="gray underline")
-
             results.append(pprint_str)
 
         if should_stack_grow_down:
@@ -40,9 +42,7 @@ class CurrentFrameStack(GenericCommand):
             print(titlify("Stack bottom (lower address)"))
         else:
             print(titlify("Stack top (higher address)"))
-
         return
-
 
 
 register_external_command(CurrentFrameStack())
