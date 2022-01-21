07e7e4f09b21f1f82f7f77d85cfaac19e628c2b7
Simon Wörner <git@simon-woerner.de>
2022-01-21 13:26:42 +0100

    add Cortex-M specific flag table

--- archs/arm-cortex-m.py
+++ archs/arm-cortex-m.py
@@ -7,11 +7,18 @@ Original PR: https://github.com/hugsy/gef/pull/651
 Author: SWW13
 """
 
+
 @register_architecture
 class ARM_M(ARM):
     arch = "ARM-M"
     aliases = ("ARM-M", )
 
-    all_registers = ARM.all_registers[:-1] + ["$xpsr",]
+    all_registers = ARM.all_registers[:-1] + ["$xpsr", ]
     flag_register = "$xpsr"
-
+    flags_table = {
+        31: "negative",
+        30: "zero",
+        29: "carry",
+        28: "overflow",
+        24: "thumb",
+    }
