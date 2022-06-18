3549ce10d578d283f42d80abb7190e34491cf5b7
hugsy <hugsy@blah.cat>
2022-06-18 12:09:50 -0700

    fixed `peekpointers`

--- scripts/peekpointers.py
+++ scripts/peekpointers.py
@@ -1,13 +1,25 @@
+
+__AUTHOR__ = "bkth"
+__VERSION__ = 0.2
+__LICENSE__ = "MIT"
+
+import pathlib
+from typing import TYPE_CHECKING, List
+
+if TYPE_CHECKING:
+    from . import *
+
+
 @register
 class PeekPointers(GenericCommand):
     """Command to help find pointers belonging to other memory regions helpful in case
     of OOB Read when looking for specific pointers"""
 
     _cmdline_ = "peek-pointers"
-    _syntax_ = "{:s} starting_address <object_name> <all>".format(_cmdline_)
+    _syntax_ = f"{_cmdline_} starting_address <object_name> <all>"
 
     @only_if_gdb_running
-    def do_invoke(self, argv):
+    def do_invoke(self, argv: List[str]):
         argc = len(argv)
         if argc not in (1, 2, 3):
             self.usage()
@@ -19,23 +31,26 @@ class PeekPointers(GenericCommand):
             return
 
         unique = True if "all" not in argv else False
-        vmmap = get_process_maps()
+        vmmap = gef.memory.maps
 
         if argc >= 2:
             section_name = argv[1].lower()
             if section_name == "stack":
-                sections = [(s.path, s.page_start, s.page_end) for s in vmmap if s.path == "[stack]"]
+                sections = [(s.path, s.page_start, s.page_end)
+                            for s in vmmap if s.path == "[stack]"]
             elif section_name == "heap":
-                sections = [(s.path, s.page_start, s.page_end) for s in vmmap if s.path == "[heap]"]
+                sections = [(s.path, s.page_start, s.page_end)
+                            for s in vmmap if s.path == "[heap]"]
             elif section_name != "all":
-                sections = [(s.path, s.page_start, s.page_end) for s in vmmap if section_name in s.path]
+                sections = [(s.path, s.page_start, s.page_end)
+                            for s in vmmap if section_name in s.path]
             else:
                 sections = [(s.path, s.page_start, s.page_end) for s in vmmap]
         else:
             sections = [(s.path, s.page_start, s.page_end) for s in vmmap]
 
         while addr.valid:
-            addr_value = read_int_from_memory(addr.value)
+            addr_value = gef.memory.read_integer(addr.value)
 
             if lookup_address(addr_value):
                 for i, section in enumerate(sections):
@@ -44,15 +59,16 @@ class PeekPointers(GenericCommand):
                         sym = gdb_get_location_from_symbol(addr_value)
                         sym = "<{:s}+{:04x}>".format(*sym) if sym else ''
                         if name.startswith("/"):
-                            name = os.path.basename(name)
+                            name = pathlib.Path(name)
                         elif not name:
-                            name = get_filename()
+                            name = gef.session.file
 
-                        ok(" Found pointer at 0x{:x} to 0x{:x} {:s} ('{:s}', perm: {:s})".format(addr.value, addr_value, sym, name, str(addr.section.permission), ))
+                        msg = f" Found pointer at 0x{addr.value:x} to 0x{addr_value:x} {sym} ('{name}', perm: {str(addr.section.permission)})"
+                        ok(msg)
 
                         if unique:
                             del sections[i]
                         break
 
-            addr = lookup_address(addr.value + current_arch.ptrsize)
+            addr = lookup_address(addr.value + gef.arch.ptrsize)
         return
