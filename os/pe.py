14b668f45c46a7cc4fda491abfd69ed9032c76db
hugsy <hugsy@blah.cat>
2022-06-23 15:07:41 -0700

    fixed os/pe format

--- os/pe.py
+++ os/pe.py
@@ -5,64 +5,89 @@ To use:
 
 ```
 gef➤  source /path/to/gef-extras/os/pe.py
-gef➤  pi gef.binary = PE(get_filepath())
-gef➤  pi reset_architecture()
 ```
 """
 
+
+__AUTHOR__ = "hugsy"
+__VERSION__ = 0.2
+__LICENSE__ = "MIT"
+
+
 import enum
-import os
 import pathlib
 import struct
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
 
-from functools import lru_cache
-from typing import Dict, Tuple, Any, Optional
+if TYPE_CHECKING:
+    from ..scripts import *
+    from ..scripts import gdb
 
-class PE:
-    """Basic PE parsing.
-    Ref:
-    - https://hshrzd.wordpress.com/pe-bear/
-    - https://blog.kowalczyk.info/articles/pefileformat.html
-    """
+
+class Pe(FileFormat):
+    """Basic PE parsing."""
+
     class Constants(enum.IntEnum):
-        DOS_MAGIC           = 0x4D5A
-        NT_MAGIC            = 0x4550
+        DOS_MAGIC = 0x4d5a
+        NT_MAGIC = 0x5045
 
     class MachineType(enum.IntEnum):
-        X86_64              = 0x8664
-        X86_32              = 0x14c
-        ARM                 = 0x1c0
-        ARM64               = 0xaa64
+        UNKNOWN = 0
+        I386 = 332
+        R4000 = 358
+        WCEMIPSV2 = 361
+        ALPHA = 388
+        SH3 = 418
+        SH3DSP = 419
+        SH4 = 422
+        SH5 = 424
+        ARM = 448
+        THUMB = 450
+        ARMNT = 452
+        AM33 = 467
+        POWERPC = 496
+        POWERPCFP = 497
+        IA64 = 512
+        MIPS16 = 614
+        MIPSFPU = 870
+        MIPSFPU16 = 1126
+        EBC = 3772
+        RISCV32 = 20530
+        RISCV64 = 20580
+        RISCV128 = 20776
+        AMD64 = 34404
+        M32R = 36929
+        ARM64 = 43620
 
     class DosHeader:
-        e_magic : int
-        e_cblp : int
-        e_cp : int
-        e_crlc : int
-        e_cparhdr : int
-        e_minalloc : int
-        e_maxalloc : int
-        e_ss : int
-        e_sp : int
-        e_csum : int
-        e_ip : int
-        e_cs : int
-        e_lfarlc : int
-        e_ovno : int
-        e_res : bytes
-        e_oemid : int
-        e_oeminfo : int
-        e_res2 : bytes
-        e_lfanew : int
+        e_magic: int
+        e_cblp: int
+        e_cp: int
+        e_crlc: int
+        e_cparhdr: int
+        e_minalloc: int
+        e_maxalloc: int
+        e_ss: int
+        e_sp: int
+        e_csum: int
+        e_ip: int
+        e_cs: int
+        e_lfarlc: int
+        e_ovno: int
+        e_res: bytes
+        e_oemid: int
+        e_oeminfo: int
+        e_res2: bytes
+        e_lfanew: int
 
     class ImageFileHeader:
-        Machine: "PE.MachineType"
+        Machine: "Pe.MachineType"
         NumberOfSections: int
         TimeDateStamp: int
         PointerToSymbolTable: int
         NumberOfSymbols: int
         SizeOfOptionalHeader: int
-        Characteristics: "PE.DllCharacteristics"
+        Characteristics: "Pe.FileCharacteristics"
 
     class OptionalHeader:
         Magic: int
@@ -95,13 +120,13 @@ class PE:
         SizeOfHeapCommit: int
         LoaderFlags: int
         NumberOfRvaAndSizes: int
-        DataDirectory: Tuple["PE.DataDirectory"]
+        DataDirectory: Tuple["Pe.DataDirectory"]
 
     class DataDirectory:
         VirtualAddress: int
         Size: int
 
-    class DllCharacteristics(enum.IntFlag):
+    class FileCharacteristics(enum.IntFlag):
         IMAGE_FILE_RELOCS_STRIPPED = 0x0001
         IMAGE_FILE_EXECUTABLE_IMAGE = 0x0002
         IMAGE_FILE_LINE_NUMS_STRIPPED = 0x0004
@@ -121,97 +146,147 @@ class PE:
         def __str__(self) -> str:
             return super().__str__().lstrip(self.__class__.__name__+".")
 
-    dos : DosHeader
-    file_header : ImageFileHeader
+    class DllCharacteristics(enum.IntFlag):
+        IMAGE_LIBRARY_PROCESS_INIT = 0x0001
+        IMAGE_LIBRARY_PROCESS_TERM = 0x0002
+        IMAGE_LIBRARY_THREAD_INIT = 0x0004
+        IMAGE_LIBRARY_THREAD_TERM = 0x0008
+        IMAGE_DLLCHARACTERISTICS_10h = 0x0010
+        IMAGE_DLLCHARACTERISTICS_20h = 0x0020
+        IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE = 0x0040
+        IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY = 0x0080
+        IMAGE_DLLCHARACTERISTICS_NX_COMPAT = 0x0100
+        IMAGE_DLLCHARACTERISTICS_NO_ISOLATION = 0x0200
+        IMAGE_DLLCHARACTERISTICS_NO_SEH = 0x0400
+        IMAGE_DLLCHARACTERISTICS_NO_BIND = 0x0800
+        IMAGE_DLLCHARACTERISTICS_1000h = 0x1000
+        IMAGE_DLLCHARACTERISTICS_WDM_DRIVER = 0x2000
+        IMAGE_DLLCHARACTERISTICS_4000h = 0x4000
+        IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE = 0x8000
+
+        def __str__(self) -> str:
+            return super().__str__().lstrip(self.__class__.__name__+".")
+
+    dos: DosHeader
+    file_header: ImageFileHeader
     optional_header: OptionalHeader
-    entry_point : int
+    shdrs: List["Shdr"]
+    name: str = "PE"
+    __entry_point: Optional[int] = None
+    __checksec: Dict[str, bool]
 
-    def __init__(self, pe: str) -> None:
-        self.fpath = pathlib.Path(pe).expanduser().resolve()
+    def __init__(self, path: Union[str, pathlib.Path]) -> None:
+        """Instantiate a PE object. A valid PE must be provided, or an exception will be thrown."""
 
-        if not os.access(self.fpath, os.R_OK):
-            raise FileNotFoundError(f"'{self.fpath}' not found/readable, most gef features will not work")
+        if isinstance(path, str):
+            self.path = pathlib.Path(path).expanduser()
+        elif isinstance(path, pathlib.Path):
+            self.path = path
+        else:
+            raise TypeError
+
+        if not self.path.exists():
+            raise FileNotFoundError(
+                f"'{self.path}' not found/readable, most gef features will not work")
+
+        self.__checksec = {}
 
         endian = gef.arch.endianness
-        with self.fpath.open("rb") as self.fd:
+        with self.path.open("rb") as self.fd:
             # Parse IMAGE_DOS_HEADER
             self.dos = self.DosHeader()
 
             self.dos.e_magic = self.read_and_unpack("!H")[0]
-            if self.dos.e_magic != PE.Constants.DOS_MAGIC:
-                raise Exception(f"Corrupted PE file (bad DOS magic, expected '{PE.Constants.DOS_MAGIC:x}', got '{self.dos.e_magic:x}'")
+            if self.dos.e_magic != Pe.Constants.DOS_MAGIC:
+                raise RuntimeError(
+                    f"Corrupted DOS file (bad DOS magic, expected '{Pe.Constants.DOS_MAGIC:x}', got '{self.dos.e_magic:x}'")
 
             self.fd.seek(0x3c, 0)
             self.dos.e_lfanew = u32(self.fd.read(4))
 
             self.fd.seek(self.dos.e_lfanew, 0)
-            pe_magic = u32(self.fd.read(4))
-            if pe_magic != PE.Constants.NT_MAGIC:
-                raise Exception("Corrupted PE file (bad PE magic)")
+            if u16(self.fd.read(2), e=Endianness.BIG_ENDIAN) != Pe.Constants.NT_MAGIC:
+                raise RuntimeError("Corrupted PE file (bad PE magic)")
 
             # Parse IMAGE_FILE_HEADER
             self.file_header = self.ImageFileHeader()
 
-            machine, self.file_header.NumberOfSections = self.read_and_unpack(f"{endian}HH")
-            self.file_header.Machine = PE.MachineType(machine)
+            machine, self.file_header.NumberOfSections = self.read_and_unpack(
+                f"{endian}HH")
+            self.file_header.Machine = Pe.MachineType(machine)
 
             self.file_header.TimeDateStamp, self.file_header.PointerToSymbolTable, \
                 self.file_header.NumberOfSymbols, self.file_header.SizeOfOptionalHeader, \
-                dll_characteristics = self.read_and_unpack(f"{endian}IIIHH")
+                pe_characteristics = self.read_and_unpack(f"{endian}IIIHH")
 
-            self.file_header.Characteristics = PE.DllCharacteristics(dll_characteristics)
+            self.file_header.Characteristics = Pe.FileCharacteristics(
+                pe_characteristics)
 
             # Parse IMAGE_OPTIONAL_HEADER
             self.optional_header = self.OptionalHeader()
 
             self.fd.seek(0x10, 1)
 
-            self.optional_header.AddressOfEntryPoint, self.optional_header.BaseOfCode,\
-            self.optional_header.BaseOfData, self.optional_header.ImageBase = self.read_and_unpack(f"{endian}IIII")
+            for attr in (
+                "AddressOfEntryPoint",
+                "BaseOfCode",
+                "BaseOfData",
+                "ImageBase",
+                "SectionAlignment",
+                "FileAlignment",
+                "MajorOperatingSystemVersion",
+                "MinorOperatingSystemVersion",
+                "MajorImageVersion",
+                "MinorImageVersion",
+                "MajorSubsystemVersion",
+                "MinorSubsystemVersion",
+                "Reserved1",
+                "SizeOfImage",
+                "SizeOfHeaders",
+                "CheckSum",
+                "Subsystem",
+                "DllCharacteristics",
+                "SizeOfStackReserve",
+                "SizeOfStackCommit",
+                "SizeOfHeapReserve",
+                "SizeOfHeapCommit",
+                "LoaderFlags",
+                "NumberOfRvaAndSizes",
+            ):
+                value = self.read_and_unpack(f"{endian}I")[0]
+                setattr(self.optional_header, attr, value)
 
-            self.entry_point = self.optional_header.AddressOfEntryPoint
+            self.__entry_point = self.optional_header.AddressOfEntryPoint
         return
 
     def __str__(self) -> str:
-        return f"PE('{self.fpath.absolute()}', {self.file_header.Machine.name}, {str(self.file_header.Characteristics)})"
+        return f"PE('{self.path.absolute()}', {self.file_header.Machine.name}, {str(self.file_header.Characteristics)})"
+
+    def seek(self, off: int) -> None:
+        self.fd.seek(off, 0)
 
     def read_and_unpack(self, fmt: str) -> Tuple[Any, ...]:
         size = struct.calcsize(fmt)
         data = self.fd.read(size)
         return struct.unpack(fmt, data)
 
-#
-# redirect calls
-#
-@lru_cache()
-def get_elf_headers(filename: str = None) -> PE:
-    """Return an PE object with info from `filename`. If not provided, will return
-    the currently debugged file."""
-    return PE(filename) if filename else PE(str(gef.session.file.absolute()))
+    @ classmethod
+    def is_valid(cls, path: pathlib.Path) -> bool:
+        return u16(path.open("rb").read(2), e=Endianness.BIG_ENDIAN) == Pe.Constants.DOS_MAGIC
 
+    @ property
+    def checksec(self) -> Dict[str, bool]:
+        warn("`checksec` doesn't apply for PE files")
+        if not self.__checksec:
+            self.__checksec["dynamic_base"] = self.optional_header.DllCharacteristics & Pe.DllCharacteristics.IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE != 0
+            self.__checksec["force_integrity"] = self.optional_header.DllCharacteristics & Pe.DllCharacteristics.IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY != 0
+            self.__checksec["nx_compat"] = self.optional_header.DllCharacteristics & Pe.DllCharacteristics.IMAGE_DLLCHARACTERISTICS_NX_COMPAT != 0
+            self.__checksec["no_isolation"] = self.optional_header.DllCharacteristics & Pe.DllCharacteristics.IMAGE_DLLCHARACTERISTICS_NO_ISOLATION != 0
+            self.__checksec["no_seh"] = self.optional_header.DllCharacteristics & Pe.DllCharacteristics.IMAGE_DLLCHARACTERISTICS_NO_SEH != 0
+        return self.__checksec
 
-def checksec(filename: str) -> Dict[str, bool]:
-    warn("`checksec` doesn't apply for PE files")
-    return {"Canary": False, "NX": False, "PIE": False, "Fortify": False}
-
-
-elf_reset_architecture = reset_architecture
-
-def pe_reset_architecture(arch: Optional[str] = None, default: Optional[str] = None) -> None:
-    if gef.binary.file_header.Machine == PE.MachineType.X86_32:
-        gef.arch = __registered_architectures__.get(Elf.Abi.X86_32)()
-    elif gef.binary.file_header.Machine == PE.MachineType.X86_64:
-        gef.arch = __registered_architectures__.get(Elf.Abi.X86_64)()
-    else:
-        raise Exception("unknown architecture")
-    return
-
-def reset_architecture(arch: Optional[str] = None, default: Optional[str] = None) -> None:
-    if isinstance(gef.binary, PE):
-        pe_reset_architecture(arch, default)
-    elif isinstance(gef.binary, ELF):
-        elf_reset_architecture(arch, default)
-    else:
-        raise Exception("unknown architecture")
-    return
-
+    @ property
+    def entry_point(self) -> int:
+        if self.__entry_point is None:
+            raise RuntimeError("PE parsing failed to retrieve the entry point")
+        return self.__entry_point
