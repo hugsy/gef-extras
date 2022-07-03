cb886d0a666d03e01c0688c1257cfd506c460a67
hugsy <hugsy@blah.cat>
2022-07-02 18:30:08 -0700

    fixed `cs-dis` too

--- scripts/__init__.pyi
+++ scripts/__init__.pyi
@@ -1647,7 +1647,7 @@ class GefHeapManager(GefManager):
 
 class GefSetting:
     def __init__(self, value: Any,
-                 cls: Optional[type] = None, description: Optional[str] = None) -> None: ...
+                 cls: Optional[type] = None, description: Optional[str] = None, hooks: Optional[Dict[str, Callable]] = None) -> None: ...
 
 
 class GefSettingsManager(dict):
